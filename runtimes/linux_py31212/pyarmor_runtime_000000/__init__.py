64b6ebf5f8f8bc6e4ae7626fc9f641b14fa95727
phoenix0x1 <phoenix0x.web3@gmail.com>
2025-12-17 15:50:14 +0400

    Disable "Withdraw and Deposit SET asset via OmniSet" quest since it's not working in UI

--- runtimes/linux_py31212/pyarmor_runtime_000000/__init__.py
+++ runtimes/linux_py31212/pyarmor_runtime_000000/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.2.3 (trial), 000000, 2025-12-17T11:31:26.269290
+# Pyarmor 9.2.3 (trial), 000000, 2025-12-17T11:49:22.560056
 from .pyarmor_runtime import __pyarmor__
--- runtimes/windows_py31210/pyarmor_runtime_000000/__init__.py
+++ runtimes/windows_py31210/pyarmor_runtime_000000/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.2.3 (trial), 000000, 2025-12-17T11:31:44.458126
+# Pyarmor 9.2.3 (trial), 000000, 2025-12-17T11:49:39.650678
 from .pyarmor_runtime import __pyarmor__
