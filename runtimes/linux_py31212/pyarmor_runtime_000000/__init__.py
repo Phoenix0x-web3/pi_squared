090ded4f28cdb8707b280c33acec49f9f8cdaf34
phoenix0x1 <phoenix0x.web3@gmail.com>
2025-12-17 20:54:07 +0400

    Minor: Fix quests

--- runtimes/linux_py31212/pyarmor_runtime_000000/__init__.py
+++ runtimes/linux_py31212/pyarmor_runtime_000000/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.2.3 (trial), 000000, 2025-12-17T11:49:22.560056
+# Pyarmor 9.2.3 (trial), 000000, 2025-12-17T16:52:54.449138
 from .pyarmor_runtime import __pyarmor__
--- runtimes/macos_py31210/pyarmor_runtime_000000/__init__.py
+++ runtimes/macos_py31210/pyarmor_runtime_000000/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.2.3 (trial), 000000, 2025-12-17T11:49:28.622282
+# Pyarmor 9.2.3 (trial), 000000, 2025-12-17T16:52:58.980147
 from .pyarmor_runtime import __pyarmor__
